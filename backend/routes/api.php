cfb4d318eaf8b419a20ed60de123fe237e08c413
Ana <anaborovina91@gmail.com>
2024-02-06 20:49:31 +0100

    Registracija, login, logout, dodeljivanje uloga

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
@@ -24,11 +25,27 @@ Route::get('/properties/search',[PropertyController::class,'search']);
 Route::get('/properties',[PropertyController::class,'index']);
 Route::get('/properties/{id}',[PropertyController::class,'show']);
 
-Route::delete('/properties/{id}',[PropertyController::class,'destroy']);
-Route::post('/properties',[PropertyController::class,'store']);
-Route::put('/properties/{id}',[PropertyController::class,'update']);
 
-Route::resource('/purchase',PurchaseController::class);
+ 
 
-Route::resource('/rating',RatingController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
 
+    Route::get('/user', [AuthController::class, 'user']); 
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    
+    Route::middleware(['checkrole:prodavac'])->group(function () {
+        Route::delete('/properties/{id}',[PropertyController::class,'destroy']);
+        Route::post('/properties',[PropertyController::class,'store']);
+        Route::put('/properties/{id}',[PropertyController::class,'update']);
+    
+        Route::resource('/purchase',PurchaseController::class);
+    });
+
+    Route::middleware(['checkrole:kupac'])->group(function () {
+        Route::resource('/rating',RatingController::class);
+    });
+
+
+});
